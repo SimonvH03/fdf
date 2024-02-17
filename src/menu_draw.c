0afe27361eac52112e5820adae1bc3479c0289c5
simon <si.hoogstraaten86@gmail.com>
2024-02-17 19:18:57 +0100

    added Menu text

--- src/menu_draw.c
+++ src/menu_draw.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   menu_draw.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svan-hoo <svan-hoo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 14:10:50 by simon             #+#    #+#             */
-/*   Updated: 2024/02/16 17:35:59 by svan-hoo         ###   ########.fr       */
+/*   Updated: 2024/02/17 19:16:37 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,24 @@
 
 void	menu_print(t_fdf *fdf)
 {
-	const int	margin = 48;
+	const int	margin = 72;
 	const int	line = 24;
 	const int	tab = 24;
+	int			x;
 	int			y;
 
-	y = margin - line;
-	mlx_put_string(fdf->mlx, "Menu", margin, (y += line));
-	mlx_put_string(fdf->mlx, "subtext", margin + tab, (y += line));
+	mlx_put_string(fdf->mlx, fdf->map->name, x = margin, y = margin);
+	mlx_put_string(fdf->mlx, "Controls", x, y += margin);
+	mlx_put_string(fdf->mlx, "Zoom:	scroll UP/DOWN", x, y += line);
+	mlx_put_string(fdf->mlx, "Pitch:	W/S", x, y += line);
+	mlx_put_string(fdf->mlx, "Yaw:	A/D", x, y += line);
+	mlx_put_string(fdf->mlx, "Roll:	Q/E", x, y += line);
+	mlx_put_string(fdf->mlx, "Presets", x, y += margin);
+	mlx_put_string(fdf->mlx, "Isometric view:	I", x, y += line);
+	mlx_put_string(fdf->mlx, "Parallel view:	P", x, y += line);
+	mlx_put_string(fdf->mlx, "Other", x, y += margin);
+	mlx_put_string(fdf->mlx, "Toggle spin:	SPACEBAR", x, y += line);
+	mlx_put_string(fdf->mlx, "Reset zoom:	M", x, y += line);
 }
 
 void	menu_draw(t_fdf *fdf)
