f07ff5dbd1fc94bdb2b9b09fba34b250f7f247c4
Simon <svan-hoo@student.codam>
2024-03-09 19:09:48 +0100

    started map_iteration, changed x,y in draw_background to i,j

--- src/map_iteration.c
+++ src/map_iteration.c
@@ -1,49 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils_draw.c                                       :+:      :+:    :+:   */
+/*   map_iteration.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: svan-hoo <svan-hoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/04 22:26:03 by simon             #+#    #+#             */
-/*   Updated: 2024/03/09 19:03:17 by svan-hoo         ###   ########.fr       */
+/*   Created: 2024/03/09 18:47:59 by svan-hoo          #+#    #+#             */
+/*   Updated: 2024/03/09 19:06:14 by svan-hoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
 
-// from fdf_draw.c, menu_draw.c
+// this function applies the function *f(whatever) to the whole map(s)
 void
-	draw_background(
-		mlx_image_t	*image,
-		uint32_t	colour)
-{
-	uint32_t	i;
-	uint32_t	j;
-
-	j = 0;
-	while (j < image->height)
+	map_iteration(t_map *map, void (*f)(void *param))
 	{
-		i = 0;
-		while (i < image->width)
-		{
-			mlx_put_pixel((mlx_image_t *)image, i, j, colour);
-			i++;
-		}
-		j++;
-	}
-}
 
-int
-	over_the_horizon(
-		t_fdf	*fdf,
-		t_line	*line)
-{
-	if (fdf->ballin == false)
-		return (0);
-	if (line->p0->z < 0 && line->p1->z < 0)
-	{
-		return (1);
 	}
-	return (0);
-}
+
+
+void	fdf_draw(void *param);
+void	fdf_colour(t_fdf *fdf);
+void	fdf_project_optimized(t_fdf *fdf);
+void	fdf_project(t_fdf *fdf);
+void	fdf_scale(t_fdf *fdf);
+void	map_set_original(t_map *map);
+void	map_fill_polar(t_map *map);
+void	map_set_polar(t_map *map);
+void	map_find_z_min_max(t_map *map);
